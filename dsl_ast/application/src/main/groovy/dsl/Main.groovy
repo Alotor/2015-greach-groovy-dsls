2d39d7970389004225729194ba8d91193fbab3a3
alonso.torres <alonso.torres@kaleidos.net>
2015-04-04 18:18:52 +0200

    AST STEP 3 - AST Table working version

--- dsl_ast/application/src/main/groovy/dsl/Main.groovy
+++ dsl_ast/application/src/main/groovy/dsl/Main.groovy
@@ -3,16 +3,17 @@ package dsl
 class Main {
     @SpockTable
     def getTable() {
-        value1 | value2  || max
-        1      | 2       || 2
-        2      | 1       || 2
-        2      | 2       || 2
+        value1 | value2 | value3 || max
+        1      | 2      | 3      || 3
+        2      | 1      | 0      || 2
+        2      | 2      | 1      || 2
     }
 
     public static void main(def args) {
         def tableData = new Main().getTable()
         println ">> ${tableData['value1']}"
         println ">> ${tableData['value2']}"
+        println ">> ${tableData['value3']}"
         println ">> ${tableData['max']}"
     }
 }
--- dsl_ast/ast/src/main/groovy/dsl/SpockTableASTTransformation.groovy
+++ dsl_ast/ast/src/main/groovy/dsl/SpockTableASTTransformation.groovy
@@ -13,13 +13,11 @@ class SpockTableASTTransformation implements ASTTransformation {
     void visit(ASTNode[] nodes, SourceUnit sourceUnit) {
         MethodNode method = (MethodNode) nodes[1]
         def existingStatements = ((BlockStatement)method.code).statements
-        existingStatements.clear()
 
-        def mapToSet = [
-            value1: [1, 2, 2],
-            value2: [2, 1, 2],
-            max: [2, 2, 2]
-        ]
+        def headers = processTableHeaders(existingStatements[0])
+        def mapToSet = processTableBody(headers, existingStatements[1..-1])
+
+        existingStatements.clear()
 
         def mapExpression = createMapStatement(mapToSet)
         existingStatements.add(mapExpression)
@@ -39,4 +37,32 @@ class SpockTableASTTransformation implements ASTTransformation {
             new MapExpression(entries)
         )
     }
+
+    def processTableHeaders(Statement headerStatement) {
+        return _preOrderBinary([], headerStatement.expression)
+    }
+
+    def processTableBody(List headers, List<Statement> bodyStatements) {
+        def currentMap = [:]
+        def rows = bodyStatements.collect { _preOrderBinary([], it.expression) }
+        headers.eachWithIndex { item, index->
+            currentMap[item] = rows.collect { it[index] }
+        }
+        return currentMap
+    }
+
+    def _preOrderBinary(List acc, def expression) {
+        if (expression instanceof VariableExpression) {
+            acc.push(expression.name)
+        } else if (expression instanceof ConstantExpression) {
+            acc.push(expression.value)
+        } else if(expression instanceof BinaryExpression){
+            _preOrderBinary(acc, expression.leftExpression)
+            _preOrderBinary(acc, expression.rightExpression)
+        } else {
+            throw new RuntimeException("Error of type: ${expression.class}")
+        }
+        return acc
+    }
+
 }
